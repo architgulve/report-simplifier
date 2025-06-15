81ccf1fc51c341c83c8fba5db4595eb28fae9e36
Archit Gulve <architgulve@gmail.com>
2025-06-16 00:05:21 +0530

    Update Tailwind CSS configuration to include all JavaScript and TypeScript files, and enhance Home screen layout with improved styling and structure for health tracking features.

--- cura/app/(tabs)/index_hm.jsx
+++ cura/app/(tabs)/index_hm.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   }
 
   return (
-    <SafeAreaView edges={["top"]} className="h-full bg-[#ffffff]">
+    <SafeAreaView edges={["top"]} className="h-full bg-white">
       <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
       <View
         style={{
@@ -27,7 +27,7 @@ const Home = () => {
           backgroundColor: "#ffffff",
         }}
       >
-        {/* // Header Section */}
+
         <Image source={require("../../assets/images/homepageicon.png")} />
         <Text
           style={{
@@ -50,243 +50,269 @@ const Home = () => {
           />
         </View>
       </View>
-      <ScrollView className="h-full bg-[#DFF6FB]">
-        <View style={{ backgroundColor: "#DFF6FB", height: "100%" }}>
-          <View
-            style={{
-              margin: 25,
-              padding: 5,
-              alignItems: "center",
-              borderRadius: 10,
-            }}
-          >
-            <Text style={{ fontSize: 24 }}>{greeting}</Text>
-            <Text stle={{ fontSize: 16, lineHeight: 30 }}>
-              Let's keep track of your Health today
-            </Text>
-          </View>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-around" }}
-          >
+      <View style={{ 
+        backgroundColor: "#DFF6FB",
+        flex: 1,
+      }}
+      >
+        <ScrollView style={{
+          flex:1
+        }}>
+          <View style={{ 
+            // height: "100%",
+            flex: 1,
+           }}>
             <View
               style={{
+                margin: 25,
+                padding: 5,
                 alignItems: "center",
-                backgroundColor: "#008CDB",
-                padding: 25,
                 borderRadius: 10,
-                flexDirection: "row",
               }}
             >
-              <Icon name="heartbeat" size={25} color="white" />
-              <View style={{ marginLeft: 10, lineHeight: 30 }}>
-                <Text
-                  style={{
-                    fontWeight: "bold",
-                    lineHeight: 30,
-                    marginTop: 0,
-                    color: "white",
-                  }}
-                >
-                  Heart Rate
-                </Text>
-                <Text
-                  style={{
-                    fontWeight: "bold",
-                    lineHeight: 30,
-                    marginTop: 0,
-                    color: "white",
-                    fontSize: 20,
-                  }}
-                >
-                  72bpm
-                </Text>
-              </View>
-            </View>
-            <View
-              style={{
-                alignItems: "center",
-                backgroundColor: "#27D240",
-                padding: 20,
-                borderRadius: 10,
-                flexDirection: "row",
-              }}
-            >
-              <Ionicons name="walk" size={30} color="white" />
-              <View style={{ marginLeft: 10, lineHeight: 30 }}>
-                <Text
-                  style={{
-                    fontWeight: "bold",
-                    lineHeight: 30,
-                    marginTop: 0,
-                    color: "white",
-                  }}
-                >
-                  Steps Today
-                </Text>
-                <Text
-                  style={{ fontWeight: "bold", fontSize: 20, color: "white" }}
-                >
-                  2,500
-                </Text>
-              </View>
-            </View>
-          </View>
-          <View
-            style={{
-              widht: "100%",
-              height: "auto",
-              margin: 15,
-              backgroundColor: "white",
-              borderRadius: 10,
-              padding: 10,
-            }}
-          >
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Ionicons name="alarm-outline" size={30} color="black" />
-              <Text
-                style={{ fontSize: 20, fontWeight: "bold", marginLeft: 10 }}
-              >
-                Medication Reminders
+              <Text style={{ fontSize: 24 }}>{greeting}</Text>
+              <Text stle={{ fontSize: 16, lineHeight: 30 }}>
+                Let's keep track of your Health today
               </Text>
             </View>
-            <View>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-around" }}
+            >
               <View
                 style={{
-                  backgroundColor: "#F0F0F0",
-                  widht: "100%",
-                  height: 70,
-                  marginTop: 10,
+                  alignItems: "center",
+                  backgroundColor: "#008CDB",
+                  padding: 25,
                   borderRadius: 10,
                   flexDirection: "row",
-                  alignItems: "center",
-                  paddingLeft: 10,
                 }}
               >
-                <View
-                  style={{
-                    width: "55%",
-                    height: 50,
-                    alignItems: "left",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                  }}
-                >
-                  <Text>Lisinopril</Text>
-                  <Text>02:00pm</Text>
-                </View>
-                <View
-                  style={{
-                    width: "45%",
-                    height: 45,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "#FFB6C1",
-                    borderRadius: 10,
-                    padding: 5,
-                  }}
-                >
-                  <Text style={{ color: "darkred", fontWeight: "bold" }}>
-                    Due Now
+                <Icon name="heartbeat" size={25} color="white" />
+                <View style={{ marginLeft: 10, lineHeight: 30 }}>
+                  <Text
+                    style={{
+                      fontWeight: "bold",
+                      lineHeight: 30,
+                      marginTop: 0,
+                      color: "white",
+                    }}
+                  >
+                    Heart Rate
+                  </Text>
+                  <Text
+                    style={{
+                      fontWeight: "bold",
+                      lineHeight: 30,
+                      marginTop: 0,
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    72bpm
                   </Text>
                 </View>
               </View>
               <View
                 style={{
-                  backgroundColor: "#F0F0F0",
-                  widht: "100%",
-                  height: 70,
-                  marginTop: 10,
+                  alignItems: "center",
+                  backgroundColor: "#27D240",
+                  padding: 20,
                   borderRadius: 10,
                   flexDirection: "row",
-                  alignItems: "center",
-                  paddingLeft: 10,
                 }}
               >
-                <View
-                  style={{
-                    width: "55%",
-                    height: 50,
-                    alignItems: "left",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                  }}
-                >
-                  <Text>Metformin</Text>
-                  <Text>09:00pm</Text>
-                </View>
-                <View
-                  style={{
-                    width: "45%",
-                    height: 45,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "#C0C0C0",
-                    borderRadius: 10,
-                    padding: 5,
-                  }}
-                >
-                  <Text style={{ color: "grey", fontWeight: "bold" }}>
-                    Upcoming
+                <Ionicons name="walk" size={30} color="white" />
+                <View style={{ marginLeft: 10, lineHeight: 30 }}>
+                  <Text
+                    style={{
+                      fontWeight: "bold",
+                      lineHeight: 30,
+                      marginTop: 0,
+                      color: "white",
+                    }}
+                  >
+                    Steps Today
+                  </Text>
+                  <Text
+                    style={{ fontWeight: "bold", fontSize: 20, color: "white" }}
+                  >
+                    2,500
                   </Text>
                 </View>
               </View>
+            </View>
+            <View
+              style={{
+                widht: "100%",
+                height: "auto",
+                margin: 15,
+                backgroundColor: "white",
+                borderRadius: 10,
+                padding: 10,
+              }}
+            >
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Ionicons name="alarm-outline" size={30} color="black" />
+                <Text
+                  style={{ fontSize: 20, fontWeight: "bold", marginLeft: 10 }}
+                >
+                  Medication Reminders
+                </Text>
+              </View>
+              <View>
+                <View
+                  style={{
+                    backgroundColor: "#F0F0F0",
+                    widht: "100%",
+                    height: 70,
+                    marginTop: 10,
+                    borderRadius: 10,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    paddingLeft: 10,
+                  }}
+                >
+                  <View
+                    style={{
+                      width: "55%",
+                      height: 50,
+                      alignItems: "left",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <Text>Lisinopril</Text>
+                    <Text>02:00pm</Text>
+                  </View>
+                  <View
+                    style={{
+                      width: "45%",
+                      height: 45,
+                      alignItems: "center",
+                      justifyContent: "center",
+                      backgroundColor: "#FFB6C1",
+                      borderRadius: 10,
+                      padding: 5,
+                    }}
+                  >
+                    <Text style={{ color: "darkred", fontWeight: "bold" }}>
+                      Due Now
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    backgroundColor: "#F0F0F0",
+                    widht: "100%",
+                    height: 70,
+                    marginTop: 10,
+                    borderRadius: 10,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    paddingLeft: 10,
+                  }}
+                >
+                  <View
+                    style={{
+                      width: "55%",
+                      height: 50,
+                      alignItems: "left",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <Text>Metformin</Text>
+                    <Text>09:00pm</Text>
+                  </View>
+                  <View
+                    style={{
+                      width: "45%",
+                      height: 45,
+                      alignItems: "center",
+                      justifyContent: "center",
+                      backgroundColor: "#C0C0C0",
+                      borderRadius: 10,
+                      padding: 5,
+                    }}
+                  >
+                    <Text style={{ color: "grey", fontWeight: "bold" }}>
+                      Upcoming
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    backgroundColor: "#F0F0F0",
+                    widht: "50%",
+                    height: 50,
+                    marginTop: 10,
+                    borderRadius: 10,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    paddingLeft: 10,
+                    alignContent: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Ionicons
+                    name="notifications-outline"
+                    size={20}
+                    color="black"
+                  />
+                  <Text>View All Reminders</Text>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                widht: "100%",
+                height: "auto",
+                margin: 10,
+                backgroundColor: "white",
+                borderRadius: 10,
+                padding: 10,
+              }}
+            >
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Ionicons name="checkbox-outline" size={20} color="black" />
+                <Text
+                  style={{ fontSize: 15, fontWeight: "bold", marginLeft: 10 }}
+                >
+                  Recent Report
+                </Text>
+              </View>
               <View
                 style={{
-                  backgroundColor: "#F0F0F0",
-                  widht: "50%",
-                  height: 50,
                   marginTop: 10,
-                  borderRadius: 10,
-                  flexDirection: "row",
+                  width: "90%",
                   alignItems: "center",
-                  paddingLeft: 10,
-                  alignContent: "center",
                   justifyContent: "center",
                 }}
               >
-                <Ionicons
-                  name="notifications-outline"
-                  size={20}
-                  color="black"
-                />
-                <Text>View All Reminders</Text>
+                <Text>
+                  Your latest health report is ready. Please review it to ensure
+                  your health is on track.
+                </Text>
               </View>
             </View>
           </View>
-          <View
-            style={{
-              widht: "100%",
-              height: "auto",
-              margin: 10,
-              backgroundColor: "white",
-              borderRadius: 10,
-              padding: 10,
-            }}
-          >
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Ionicons name="checkbox-outline" size={20} color="black" />
-              <Text
-                style={{ fontSize: 15, fontWeight: "bold", marginLeft: 10 }}
-              >
-                Recent Report
-              </Text>
-            </View>
-            <View
-              style={{
-                marginTop: 10,
-                width: "90%",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Text>
-                Your latest health report is ready. Please review it to ensure
-                your health is on track.
-              </Text>
-            </View>
-          </View>
+        </ScrollView>
+        <View
+          style={{
+            backgroundColor: "#008CDB",
+            padding: 20,
+            borderRadius: 100,
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            opacity: 0.8,
+          }}
+        >
+          <Ionicons name="add" size={32} color="black" />
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
