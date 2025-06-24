9aee0a59aa6a8405f9c2a6e9fdf0497288f14beb
ATHARVA KHEDEKAR <atharvakh7@gmail.com>
2025-06-24 13:39:09 +0400

    add medication

---
+++ cura/app/(tabs)/(medireminders)/_layout.jsx
@@ -0,0 +1,5 @@
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
