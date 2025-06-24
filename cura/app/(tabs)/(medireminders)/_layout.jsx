5394f0fac9de0e927e62a707fb9bab25d27b51c5
Archit Gulve <architgulve@gmail.com>
2025-06-24 16:17:18 +0530

    Refactor MediReminders layout and enhance navigation. Introduced MediRemindersLayout component with stack navigation for 'remind' and 'addmedications' screens. Updated MedicationReminders to navigate to 'addmedications' on button press.

--- cura/app/(tabs)/(medireminders)/_layout.jsx
+++ cura/app/(tabs)/(medireminders)/_layout.jsx
@@ -1,5 +1,14 @@
-import { Tabs } from "expo-router";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
 
+const MediRemindersLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='remind' options={{ headerShown: false }} />
+      <Stack.Screen name='addmedications' options={{ headerShown: false }} />
+    </Stack>
+  )
+}
+
+export default MediRemindersLayout
